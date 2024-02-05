1a68cd321843927b72a8aadc721b53e3bf2aeccd
Lucas <lucasbrodersen@gmail.com>
2024-02-05 21:02:23 +0000

    first commit

---
+++ job-engine/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <p>{{ jobs[0].location }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from './types/Job'
+
+let age: number
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as string | number
+    // })
+
+    // return {...toRefs(state)}
+
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+
+    // return {name, age}
+
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    return { jobs }
+  },
+  
+});
+</script>
+
+<style>
+
+</style>
